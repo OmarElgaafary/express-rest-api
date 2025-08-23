864efd1a16729f58ba4d903889f1cffd1065b9c5
Omar Adly <omarelgaafaryy@gmail.com>
2025-08-23 06:46:32 +0300

    Added controller functions

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,69 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 const users = require('../users.js')
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUserById,
+    deleteUserById
+} = require('../controller/controller.js')
 
-router.get('/users', (req, res) => {
-    if (!users) {
-        res.status(200).json({ success: true, msg: 'No users stored.' })
-    }
+router.get('/users', getUsers)
 
-    res.status(200).json({ success: true, data: users })
-})
+router.get('/:id', getUserById)
 
-router.get('/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const foundUser = users.find(user => user.id === id)
-    if (!foundUser) {
-        return res.status(404).json({ success: false, msg: `User with id: ${id} cannot be found.` })
-    }
+router.post('/', createUser)
 
-    res.status(200).json({ success: true, data: foundUser });
-})
+router.put('/:id', updateUserById)
 
-router.post('/', (req, res) => {
-    const newUser = req.body
-
-    if (!newUser) {
-        return res.status(400).json({ success: false, msg: `Error while posting request.` })
-    }
-
-    users.map(user => {
-        if (user.id === newUser.id) return res.status(409).send('User already stored.')
-    })
-
-    users.push(newUser)
-    res.status(200).json({ success: true, data: users })
-})
-
-router.put('/:id', (req, res) => {
-    const putId = Number(req.params.id)
-    const { username } = req.body
-
-    if (!putId || !username) return res.status(400).json({ success: false, msg: '404 Bad Request' })
-
-    const userExists = users.find(user => user.id === putId)
-    if (userExists) {
-        userExists.username = username
-        return res.status(200).json({ success: true, data: users, msg: 'User replaced successfully!' });
-    }
-
-    users.push({ username, id: putId });
-    res.status(200).json({ success: true, data: users });
-
-})
-
-router.delete('/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const foundUser = users.find(user => user.id === id)
-
-    if (!foundUser) {
-        return res.status(404).send('404 User not found.')
-    }
-
-    const index = users.findIndex(user => user.id === id)
-    if (index !== -1) users.splice(index, 1);
-
-    res.status(200).json({ success: true, data: users, msg: 'User deleted successfully.' })
-})
+router.delete('/:id', deleteUserById)
 
 module.exports = router
